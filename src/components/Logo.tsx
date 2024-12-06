5adbf80c8acf3d7e7f5ce0d508a453dafa9f2cec
Brandon Eichhorn <101891339+eduance@users.noreply.github.com>
2024-12-06 05:15:48 -0800

    Update src/components/Logo.tsx [BoltSync]

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,7 +8,7 @@ export function Logo() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.6 }}
-      className="fixed top-8 left-1/2 -translate-x-1/2 z-10"
+      className="fixed top-4 md:top-8 left-1/2 -translate-x-1/2 z-10"
     >
       <Link to="/" className="inline-block" aria-label="INSOLVIO - Home">
         <img 
@@ -16,7 +16,7 @@ export function Logo() {
           alt="INSOLVIO Logo - A lightbulb symbolizing innovative solutions"
           width={560}
           height={128}
-          className="h-8 w-auto"
+          className="h-6 sm:h-8 w-auto"
         />
       </Link>
     </motion.div>
